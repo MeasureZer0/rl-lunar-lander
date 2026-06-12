69bf38a71fae0f57573ff097a0c5a20d6c48866d
init <44498727+init37@users.noreply.github.com>
2026-06-12 19:49:16 +0200

    Fix/errors (#10)
    * fix: configs
    
    * chore: add dqn to optuna

--- training/optuna_search.py
+++ training/optuna_search.py
@@ -21,6 +21,24 @@ def run_trial(trial: optuna.Trial, base_config: ExperimentConfig) -> float:
             "hidden_dim", [64, 128, 256]
         )
 
+    elif config.agent.name == "dqn":
+        config.agent.dqn.learning_rate = trial.suggest_float(
+            "learning_rate", 1e-4, 1e-2, log=True
+        )
+        config.agent.dqn.gamma = trial.suggest_float("gamma", 0.90, 0.999)
+        config.agent.dqn.hidden_dim = trial.suggest_categorical(
+            "hidden_dim", [64, 128, 256]
+        )
+        config.agent.dqn.batch_size = trial.suggest_categorical(
+            "batch_size", [32, 64, 128]
+        )
+        config.agent.dqn.epsilon_decay = trial.suggest_float(
+            "epsilon_decay", 0.990, 0.9999
+        )
+        config.agent.dqn.target_update_frequency = trial.suggest_categorical(
+            "target_update_frequency", [50, 100, 200]
+        )
+
     config.wandb.enabled = False
     config.checkpoint.enabled = False
     config.env.render_mode = None
